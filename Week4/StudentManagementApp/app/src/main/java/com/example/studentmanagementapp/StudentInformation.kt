90dc8f0c05a1189a2f529b96530bc6b0bb5ff479
kygor <namworkmc@gmail.com>
2022-03-08 11:44:46 +0700

    first commit

--- Week4/StudentManagementApp/app/src/main/java/com/example/studentmanagementapp/StudentInformation.kt
+++ Week4/StudentManagementApp/app/src/main/java/com/example/studentmanagementapp/StudentInformation.kt
@@ -1,11 +1,20 @@
 package com.example.studentmanagementapp
 
+import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.RadioGroup
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 class StudentInformation : AppCompatActivity() {
 
@@ -15,6 +24,7 @@ class StudentInformation : AppCompatActivity() {
     private var genderRadioGr: RadioGroup? = null
     private var saveBtn: Button? = null
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_student_information)
@@ -26,7 +36,24 @@ class StudentInformation : AppCompatActivity() {
         saveBtn = findViewById(R.id.saveBtn)
 
         saveBtn!!.setOnClickListener {
-            Log.i("test", "hehe")
+            val genderId = genderRadioGr!!.checkedRadioButtonId
+
+            val isEmpty =
+                TextUtils.isEmpty(fullNameEditText!!.text) || TextUtils.isEmpty(dobEditText!!.text) || TextUtils.isEmpty(
+                    classIdEditText!!.text
+                ) || genderId == -1
+
+            if (isEmpty) {
+                Log.i("hehe", "Empty field")
+                Toast.makeText(this, "All fields to be required", Toast.LENGTH_SHORT).show()
+            } else {
+                val fullName = fullNameEditText!!.text.toString()
+                val dob =
+                    LocalDate.parse(dobEditText!!.text.toString(), DateTimeFormatter.ofPattern("dd/MM/yyyy"))
+                val classId = classIdEditText!!.text.toString()
+
+                Log.i("hehe", "$fullName - $dob - $classId")
+            }
         }
     }
 }
